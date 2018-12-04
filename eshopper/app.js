dc291fe522fbded6361082e13b30bb22ca12b228
uglyding2 <uglyding2@naver.com>
2018-12-04 17:19:27 +0900

    eshopper

---
+++ eshopper/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login.js'); // 로그인
+var cartRouter = require('./routes/cart.js'); // 카트
+var checkoutRouter = require('./routes/checkout.js'); // 체크아웃
+var shopRouter = require('./routes/shop.js') // product
+var ProductDetailsRouter = require('./routes/ProductDetails.js') // ProductDetails
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login', loginRouter); // 로그인
+app.use('/cart', cartRouter); // 카트
+app.use('/checkout', checkoutRouter); // 체크아웃
+app.use('/shop', shopRouter); // product
+app.use('/ProductDetails', ProductDetailsRouter); //ProductDetails
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
